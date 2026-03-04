bcd41dbf8796ab28e8b2d9a3b8552eed09824471
vishnusp7578 <69621545+vishnusp7578@users.noreply.github.com>
2026-03-04 15:47:06 +0530

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "service_account_email" {
+  value = google_service_account.github_actions.email
+}
+
+output "workload_identity_provider" {
+  value = google_iam_workload_identity_pool_provider.github_provider.name
+}
